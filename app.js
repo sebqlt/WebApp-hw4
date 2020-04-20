e8f414abb376df23d38a71e32961c9d2cd4c780f
sebqlt <sebastien.quillet@outlook.com>
2020-04-20 10:53:13 -0700

    cleaner code

--- app.js
+++ app.js
@@ -1,68 +1,30 @@
-function increase() {
-    myBallon.style.fontsize=myBallon.style.fontsize+10;
-    myFunction()
-}
+var size=30
+
 function myFunction(){
     var result=document.getElementById("result");
     var resultStyle=getComputedStyle(result)
     result.innerHTML="<p>&#127880;</p>"
     document.addEventListener("keypress", e => {
-        if(String.fromCharCode(e.charCode)==="i"||String.fromCharCode(e.charCode)==="I"){
-            if(resultStyle.fontSize==='10px'){
-                result.style.fontSize="20px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='20px'){
-                result.style.fontSize="30px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='30px'){
-            result.style.fontSize="40px"
-            result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='40px'){
-                result.style.fontSize="50px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='50px'){
-                result.style.fontSize="60px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='60px'){
-                result.style.fontSize="70px"
+        if(String.fromCharCode(e.charCode)==="i"||String.fromCharCode(e.charCode)==="I"){ 
+            size=size+10
+            result.style.fontSize=size+"px"
+            if(size>=70){
                 result.innerHTML="<p>&#128165;</p>"
-                document.removeEventListener("keypress",e);
+            }
+            else{
+            result.innerHTML="<p>&#127880;</p>"
             }
         }
-        if(String.fromCharCode(e.charCode)==="d"||String.fromCharCode(e.charCode)==="D"){
-            if(resultStyle.fontSize==='10px'){
-                result.style.fontSize="20px"
+        if(String.fromCharCode(e.charCode)==="d"||String.fromCharCode(e.charCode)==="D"){ 
+            size=size-10
+            result.style.fontSize=size+"px"
+            if(size===0){
+                result.style.fontSize="30px"
                 result.innerHTML="<p>Done</p>"
             }
-            else if(resultStyle.fontSize==='20px'){
-                result.style.fontSize="10px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='30px'){
-            result.style.fontSize="20px"
+            else{
             result.innerHTML="<p>&#127880;</p>"
             }
-            else if(resultStyle.fontSize==='40px'){
-                result.style.fontSize="30px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='50px'){
-                result.style.fontSize="40px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='60px'){
-                result.style.fontSize="50px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-            else if(resultStyle.fontSize==='70px'){
-                result.style.fontSize="60px"
-                result.innerHTML="<p>&#127880;</p>"
-            }
-        } 
+        }
       });
 }
